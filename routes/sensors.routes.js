5c122f8cc4ddd8cf37761a1145d64c2d8d951a9b
mrodih00 <mrodih00@estudiantes.unileon.es>
2025-03-07 11:45:26 +0100

    new file:   controllers/auth.controller.js 	new file:   middleware/auth.Middleware.js 	new file:   models/user.js 	modified:   package-lock.json 	modified:   package.json 	modified:   routes/sensors.routes.js 	new file:   routes/user.routes.js

--- routes/sensors.routes.js
+++ routes/sensors.routes.js
@@ -1,12 +1,13 @@
 const express = require('express');
-const { getAllSensors, createSensor, getSensorById, updateSensor, deleteSensor } = require('../controllers/sensor.controller');
-
+const { getAllSensors, createSensor, getSensorById,
+    updateSensor, deleteSensor } =
+    require('../controllers/sensor.controller');
+const authMiddleware =
+    require('../middlewares/auth.Middleware');
 const router = express.Router();
-
 router.get('/', getAllSensors);
-router.post('/', createSensor);
+router.post('/', authMiddleware, createSensor);
 router.get('/:id', getSensorById);
-router.put('/:id', updateSensor);
-router.delete('/:id', deleteSensor);
-
-module.exports = router;
+router.put('/:id', authMiddleware, updateSensor);
+router.delete('/:id', authMiddleware, deleteSensor);
+module.exports = router; 
